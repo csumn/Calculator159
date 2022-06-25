2214916d77b822407e501ef12d105cb8c1bb3cef
csumn <csumn13@gmail.com>
2022-06-25 10:33:54 +0530

    This is Sub Code

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -6,6 +6,18 @@ public class Main {
 	public static void main(String[] args) {
 		Main main = new Main();
 		main.addition();
+		main.sub();
+	}
+
+	private void sub() {
+		int a ,b;
+		@SuppressWarnings("resource")
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter two numbers : ");
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		int result  = a - b ;
+		System.out.println(result);		
 	}
 
 	public void addition() {
@@ -18,5 +30,4 @@ public class Main {
 		int result  = a +b ;
 		System.out.println(result);
 	}
-
 }
